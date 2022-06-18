0cc78bdf2cbe6958465579e26e0c56bedee108b8
Nirav <n.gondaliya@techniversesoftware.com>
2022-06-18 14:55:48 +0530

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  socketUrl: 'http://44.201.202.10:5000',
-  apiUrl: 'http://44.201.202.10:5000/api/v1/',
-  mediaUrl: 'http://44.201.202.10:5000/assets/',
+  socketUrl: 'http://3.80.209.97',
+  apiUrl: 'http://3.80.209.97/api/v1/',
+  mediaUrl: 'http://3.80.209.97/assets/',
   googleMapApiKey: 'AIzaSyAtHMP9FNoG6Z7K1uYoQS3gLXo3HSMvHHs'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   production: false,
   // apiUrl: "http://35.177.143.238:5001/api/v1/",
   // socketUrl: "http://35.177.143.238:5001",
-  socketUrl: 'http://44.201.202.10:5000',
-  apiUrl: 'http://44.201.202.10:5000/api/v1/',
-  mediaUrl: "http://44.201.202.10:5000/assets/",
+  socketUrl: 'http://3.80.209.97',
+  apiUrl: 'http://3.80.209.97/api/v1/',
+  mediaUrl: "http://3.80.209.97/assets/",
   googleMapApiKey: "AIzaSyAtHMP9FNoG6Z7K1uYoQS3gLXo3HSMvHHs",
 };
