01aee2b39986e87e7362a554cc710b43f139ac5d
Nirav <n.gondaliya@techniversesoftware.com>
2022-06-20 13:55:38 +0530

    Some error resolved

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://ec2-3-80-209-97.compute-1.amazonaws.com/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,5 +10,5 @@ export const environment = {
   socketUrl: 'http://3.80.209.97',
   apiUrl: 'http://3.80.209.97/api/v1/',
   mediaUrl: "http://3.80.209.97/assets/",
-  googleMapApiKey: "AIzaSyAtHMP9FNoG6Z7K1uYoQS3gLXo3HSMvHHs",
+  googleMapApiKey: "AIzaSyDVbeYPNp2uMiFH5-skKtq-eQeGU14POXk",
 };
